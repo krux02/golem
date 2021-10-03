fc159d5d02b9041c01594d06fb49e6287cf01cab
Arne Döring <arne.doering@gmx.net>
2021-10-03 13:39:40 +0200

    golem mode improvements

--- golem-mode.el
+++ golem-mode.el
@@ -11,7 +11,7 @@
  `(,(rx
      ;; line-start
      ;; File
-     (group-n 1 (1+ (in alnum "\\" "/" "_" "." "-")) ".golem")
+     (group-n 1 (1+ (any alnum "\\" "/" "_" "." "-")) ".golem")
      "("
      ;; Line
      (group-n 2 (1+ digit))
@@ -24,15 +24,25 @@
      (or "Error:" (group-n 5 "Warning:") (group-n 6 "Info:")))
    1 2 (3 . 4) (5 . 6)))
 
-(defconst golem-highlights
-      (list
-       (cons (rx (or "Sin" "Cos" "Sum")) font-lock-function-name-face)
-       (cons (rx (or "Pi" "Infinity")) font-lock-constant-face)))
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.golem\\'" . golem-mode))
 
+(defconst golem-highlights
+  (rx-let ((ident (seq alpha (* (any alnum "_")))))
+    (list
+     (cons (rx line-start (or "proc" "type")) font-lock-keyword-face)
+     (cons "\t+" font-lock-warning-face)
+     (list (rx (group ident) "(") 1 font-lock-function-name-face)
+     (list (rx ":" (* " ") (group ident)) 1 font-lock-type-face)
+     (list (rx (group-n 1 "struct") (* " ") "{") 1 font-lock-keyword-face)
+     )))
+
+;;;###autoload
 (define-derived-mode golem-mode prog-mode "golem"
   "major mode for editing golem language code"
-  (setq font-lock-defaults '(golem-highlights)))
-
+  (set (make-local-variable 'font-lock-defaults) '(golem-highlights))
+  (set (make-local-variable 'compile-command)
+       (concat "golem " (file-relative-name buffer-file-name))))
 
 (provide 'golem-mode)
 ;;; golem-mode.el ends here
