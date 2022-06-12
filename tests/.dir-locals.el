0216338b732d6b3968f4e3a76a3c3c7b66da27fc
Arne Döring <arne.doering@gmx.net>
2022-06-13 01:25:52 +0200

    add dir locals

---
+++ tests/.dir-locals.el
@@ -0,0 +1,6 @@
+
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((go-mode . ((compile-command . "cd .. && go test")))
+ (golem-mode . ((compile-command . "cd .. && go test"))))
